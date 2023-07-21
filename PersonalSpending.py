83b161f279a847d5221108ca073269cd0535e2db
Воробьев Иван Евгеньевич <IEVorobyev@sberbank.ru>
2023-07-21 03:15:18 +0300

    first impl

---
+++ PersonalSpending.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from datetime import datetime
+
+DF_COLUMNS = ["id", "amount", "category", "date"]
+
+class PersonalSpending:
+    def __init__(self):
+        self.spent_money_this_month = 0
+        self.spent_money_this_day = 0
+        self.all_spending = pd.DataFrame(columns=DF_COLUMNS) # todo: should be DataBase
+        self.categories = {"supermarket", "transport", "infrequent spending", "restaurant", "one per month", "travel"}
+        self.current_id = 0
+        self.today = datetime.today()
+
+    def set_categories(self, categories):
+        self.categories = categories
+
+    def add_spending(self, amount: int, category: str, date=None):
+        today = datetime.today()
+        if self.today.month != today.month:
+            self.spent_money_this_month = 0
+        if self.today != today:
+            self.today = today
+            self.spent_money_this_day = 0
+        if date is None:
+            date = today
+        new_row = {"id": self.current_id, "amount": amount, "category": category, "date": date}
+        self.all_spending.append(new_row, ignore_index=True)
+        self.current_id += 1
+        if date == today:
+            self.spent_money_this_day += amount
+        if date.month == today.month:
+            self.spent_money_this_month += amount
+
+    def undo(self, id: int):
+        undo_row = self.all_spending[self.all_spending["id"] == id]["sp"].iloc[0]
+        if undo_row["date"] == self.today:
+            self.spent_money_this_day -= undo_row["amount"]
+        if undo_row["date"].month == self.today.month:
+            self.spent_money_this_month -= undo_row["amount"]
+        self.all_spending = self.all_spending.drop(self.all_spending[self.all_spending["id"] == id].index)
+
+    def get_spending(self):
+        today = datetime.today()
+        if self.today.month != today.month:
+            self.spent_money_this_month = 0
+        if self.today != today:
+            self.today = today
+            self.spent_money_this_day = 0
+        return self.spent_money_this_day, self.spent_money_this_month / self.today.day
+
